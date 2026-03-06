889c6b8b87d2b4650b97e66e982ac6a63182519b
Anton <golvcenkoanton@gmail.com>
2026-03-06 01:51:28 +0100

    Upd IVault interface | Add timelock, roles in Vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -7,9 +7,16 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import {ErrorsLib} from "./libraries/ErrorsLib.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
+import {ErrorsLib} from "./libraries/ErrorsLib.sol";
+import {EventsLib} from "./libraries/EventsLib.sol";
+import {PendingLib, PendingUint192, PendingAddress} from "./libraries/PendingLib.sol";
+import {ConstantsLib} from "./libraries/ConstantsLib.sol";
+
+
 import {IVault} from "./interfaces/IVault.sol";
+import {IStrategy, StrategyConfig} from "./interfaces/IStrategy.sol";
 
 /// @title Vault
 /// @author @ohMySol
@@ -31,30 +38,77 @@ import {IVault} from "./interfaces/IVault.sol";
 ///   before being included in the vault.
 contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
     using SafeERC20 for IERC20;
+    using PendingLib for PendingUint192;
+    using PendingLib for PendingAddress;
 
-    /* IMMUTABLES */
+    /* STORAGE */
 
     /// @inheritdoc IVault
-    uint256 public immutable FEE_BPS;
+    uint96 public fee;
     
     /// @inheritdoc IVault
-    address public immutable FEE_RECIPIENT; 
+    address public feeRecipient; 
+
+    /// @inheritdoc IVault
+    address public curator;
+
+    /// @inheritdoc IVault
+    address public guardian;
+
+    /// @inheritdoc IVault
+    uint256 public timelock;
+    
+    /// @inheritdoc IVault
+    PendingAddress public pendingGuardian;
+
+    /// @inheritdoc IVault
+    PendingUint192 public pendingTimelock;
+
+    /// @dev List of strategies currently included in the vault.
+    address[] internal _strategies;
+
+    /// @inheritdoc IVault
+    mapping(address strategy => StrategyConfig) public strategyConfig;
+
+    /* MODIFIERS */
+
+    /// @notice Modifier restrict access only to address with guardian role.
+    modifier onlyGuardian() {
+        if (msg.sender != guardian) revert ErrorsLib.NotGuardian();
+        _;
+    }
+
+    /// @notice Modifier restrict access only to address with owner or curator role.
+    modifier onlyOwnerOrCurator() {
+        if (msg.sender != owner() && msg.sender != curator) revert ErrorsLib.NotOwnerOrCurator();
+        _;
+    }
+
+    /// @notice Modifier ensures a timelock has elapsed, before allowing the execution of the function.
+    /// @param validAt The timestamp at which the change becomes valid.
+    modifier afterTimelock(uint256 validAt) {
+        if (validAt == 0) revert ErrorsLib.NoPendingChange();
+        if (block.timestamp < validAt) revert ErrorsLib.TimelockNotElapsed();
+        _;
+    }
     
     /* CONSTRUCTOR */
 
-    /// @dev Intiializes the contract
+    /// @dev Initializes the contract
     /// @param _asset The address of the underlying asset
     /// @param _name The name of the vault
     /// @param _symbol The symbol of the vault 
     /// @param _vaultFee The vault fee in basis points
     /// @param _feeRecipient The address of the fee recipient
+    /// @param _timelock The timelock duration in seconds
     constructor(
         address _owner,
         address _asset, 
         string memory _name, 
         string memory _symbol,
         uint256 _vaultFee,
-        address _feeRecipient
+        address _feeRecipient,
+        uint256 _timelock
     )
      ERC4626(IERC20(_asset)) 
      ERC20(_name, _symbol)
@@ -62,9 +116,12 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
     {
         if (_asset == address(0)) revert ErrorsLib.ZeroAddress();
         if (_feeRecipient == address(0)) revert ErrorsLib.ZeroAddress();
-        if (_vaultFee >= 10_000) revert ErrorsLib.InvalidFeeBPS();
-        FEE_BPS = _vaultFee;
-        FEE_RECIPIENT = _feeRecipient;
+        if (_vaultFee > ConstantsLib.MAX_FEE) revert ErrorsLib.InvalidFeeBPS();
+
+        fee = uint96(_vaultFee);
+        feeRecipient = _feeRecipient;
+
+        _checkAndSetTimelock(_timelock);
     }
 
     /* ERC4626 (PUBLIC) */
@@ -72,25 +129,49 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
     /// @inheritdoc IERC4626
     /// @dev Can be paused by the owner in case of emergency.
     function deposit(uint256 assets, address receiver) public virtual override whenNotPaused returns (uint256) {
+        _accrueInterest();
         return super.deposit(assets, receiver);        
     }
 
     /// @inheritdoc IERC4626
     /// @dev Can be paused by the owner in case of emergency.
     function mint(uint256 shares, address receiver) public virtual override whenNotPaused returns (uint256) {
+       _accrueInterest();
        return super.mint(shares, receiver);
     }
 
     /// @inheritdoc IERC4626
     /// @dev Can be paused by the owner in case of emergency.
     function withdraw(uint256 assets, address receiver, address owner) public virtual override whenNotPaused returns (uint256) {
+        _accrueInterest();
         return super.withdraw(assets, receiver, owner);
     }
 
     /// @inheritdoc IERC4626
     /// @dev Can be paused by the owner in case of emergency.
     function redeem(uint256 shares, address receiver, address owner) public virtual override whenNotPaused returns (uint256) {
+        _accrueInterest();
         return super.redeem(shares, receiver, owner);
+    }
+
+    /// @inheritdoc ERC4626
+    /// @dev Returns the total managed assets including vault's idle funds and strategies balances.
+    function totalAssets() public view virtual override returns (uint256) {
+        uint256 assetsInVault = IERC20(asset()).balanceOf(address(this));
+
+        uint256 len = _strategies.length;
+        for (uint256 i = 0; i < len; ) {
+            address strategy = _strategies[i];
+            StrategyConfig memory config = strategyConfig[strategy];
+            if (config.enabled) {
+                assetsInVault += IStrategy(strategy).totalAssets();
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        return assetsInVault;
     }
 
     /* GASLESS PUBLIC FUNCTIONS */
@@ -106,6 +187,7 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
         bytes32 permitS
     ) public virtual whenNotPaused returns (uint256) {
         if (assets == 0) revert ErrorsLib.ZeroAssetsInAmount();
+        _accrueInterest();
         IERC20Permit(asset()).permit(
             owner, 
             address(this), 
@@ -130,11 +212,12 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
         bytes32 permitS
     ) public virtual whenNotPaused returns (uint256) {
         if (shares == 0) revert ErrorsLib.ZeroSharesInAmount();
-        uint256 grossAssets = previewMint(shares);
+        _accrueInterest();
+        uint256 assets = previewMint(shares);
         IERC20Permit(asset()).permit(
             owner, 
             address(this), 
-            grossAssets, 
+            assets, 
             deadline, 
             permitV, 
             permitR, 
@@ -154,6 +237,89 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
     /// @inheritdoc IVault
     function unpause() public onlyOwner {
         _unpause();
+    }
+
+    /// @inheritdoc IVault
+    function setCurator(address newCurator) external onlyOwner {
+        if (newCurator == address(0)) revert ErrorsLib.ZeroAddress();
+        if (newCurator == curator) revert ErrorsLib.AlreadySet();
+
+        curator = newCurator;
+
+        emit EventsLib.CuratorUpdated(newCurator);
+    }
+
+    /// @inheritdoc IVault
+    function submitGuardian(address newGuardian) external onlyOwner {
+        if (newGuardian == address(0)) revert ErrorsLib.ZeroAddress();
+        if (newGuardian == guardian) revert ErrorsLib.AlreadySet();
+
+        pendingGuardian.update(newGuardian, timelock);
+
+        emit EventsLib.GuardianSubmited(newGuardian);
+    }
+
+    /// @inheritdoc IVault
+    function submitTimelock(uint256 newTimelock) external onlyOwner {
+        if (newTimelock == 0) revert ErrorsLib.InvalidTimelock();
+        if (pendingTimelock.validAt != 0) revert ErrorsLib.PendingTimelockExists();
+        
+        if (newTimelock >= timelock) {
+            // If the new timelock greater than the current timelock, set it immediately
+            _checkAndSetTimelock(newTimelock);
+        } else {
+            // Safe cast to uint192 due to the of validation in `_checkAndSetTimelock` function
+            pendingTimelock.update(uint192(newTimelock), timelock);
+            emit EventsLib.TimelockSubmitted(newTimelock);
+        }
+    }
+
+     /// @inheritdoc IVault
+     function setFee(uint256 newFee) external onlyOwner {
+        if (newFee == fee) revert ErrorsLib.AlreadySet();
+        if (newFee > ConstantsLib.MAX_FEE) revert ErrorsLib.InvalidFeeBPS();
+        // Safe cast to uint96 due to the above `MAX_FEE` validation
+        fee = uint96(newFee);
+        
+        emit EventsLib.FeeUpdated(newFee);
+    }
+
+    /// @inheritdoc IVault
+    function setFeeRecipient(address newFeeRecipient) external onlyOwner {
+        if (newFeeRecipient == address(0)) revert ErrorsLib.ZeroAddress();
+        if (newFeeRecipient == feeRecipient) revert ErrorsLib.AlreadySet();
+
+        feeRecipient = newFeeRecipient;
+
+        emit EventsLib.FeeRecipientUpdated(newFeeRecipient);
+    }
+
+    /* EXTERNAL FUNCTIONS */
+
+    /// @inheritdoc IVault
+    function acceptGuardian() external afterTimelock(pendingGuardian.validAt) {
+        _setGuardian(pendingGuardian.value);
+    }
+
+    /// @inheritdoc IVault
+    function acceptTimelock() external afterTimelock(pendingTimelock.validAt) {
+        _checkAndSetTimelock(pendingTimelock.value);
+    }
+
+    /* ONLY GUARDIAN FUNCTIONS */
+
+    /// @inheritdoc IVault
+    function revokePendingGuardian() external onlyGuardian {
+        if (pendingGuardian.validAt == 0) revert ErrorsLib.NoPendingChange();
+        delete pendingGuardian;
+        emit EventsLib.PendingGuardianRevoked(msg.sender);
+    }
+
+    /// @inheritdoc IVault
+    function revokePendingTimelock() external onlyGuardian{
+        if (pendingTimelock.validAt == 0) revert ErrorsLib.NoPendingChange();
+        delete pendingTimelock;
+        emit EventsLib.PendingTimelockRevoked(msg.sender);
     }
 
     /* INTERNAL FUNCTIONS */
@@ -198,6 +364,39 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
         return assets;
     }
 
+    /// @notice Checks if the new timelock duration is valid and sets it.
+    /// @dev Set `timelock` to `newTimelock` and delete `pendingTimelock`.
+    /// @param newTimelock The new timelock duration in seconds.
+    function _checkAndSetTimelock(uint256 newTimelock) internal {
+        if (newTimelock > ConstantsLib.MAX_TIMELOCK) revert ErrorsLib.MaxTimelockExceeded();
+        if (newTimelock < ConstantsLib.MIN_TIMELOCK) revert ErrorsLib.MinTimelockNotReached();
+
+        timelock = newTimelock;
+        delete pendingTimelock;
+
+        emit EventsLib.TimelockSet(msg.sender, newTimelock);
+    }
+
+    /// @notice Sets the guardian address.
+    /// @dev Set `guardian` to `newGuardian` and delete `pendingGuardian`.
+    /// @param newGuardian The new guardian address.
+    function _setGuardian(address newGuardian) internal {
+        guardian = newGuardian;
+        delete pendingGuardian;
+        emit EventsLib.GuardianUpdated(newGuardian);
+    }
+
+    /// TODO: AWAITS REFACTORING
+    function _accrueInterest() internal {
+        _accrueFeeAndAssets();
+    }
+
+    /// TODO: AWAITS REFACTORING
+    function _accrueFeeAndAssets() internal {}
+
+    /// TODO: AWAITS REFACTORING
+    function _updateStrategySnapshots() internal {}
+
     /* ERC4626 (INTERNAL) */
 
     /// @inheritdoc ERC4626
@@ -205,4 +404,6 @@ contract Vault is ERC4626, Ownable2Step, Pausable, IVault {
     function _decimalsOffset() internal view virtual override returns (uint8) {
         return 9;
     }
+
+    
 }
--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -1,21 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+import {IStrategy, StrategyConfig} from "./IStrategy.sol";
+
 /// @title IVault
 /// @author @ohMySol
 /// @notice Interface that defines the functions for the Vault contract.
 interface IVault {
-    /// @notice Vault fee in basis points. Can not be greater than 100%(10_000 BPS).
-    function FEE_BPS() external view returns (uint256);
+    /// @notice Performance fee in basis points. Can not be greater than `MAX_FEE`.
+    function fee() external view returns (uint96);
     
-    /// @notice Vault fee recipient address. Can not be the zero address.
-    function FEE_RECIPIENT() external view returns (address);
-    
+    /// @notice Performance fee recipient address. Can not be the zero address.
+    function feeRecipient() external view returns (address);
+
+    /// @notice Address of the curator.
+    function curator() external view returns (address);
+
+    /// @notice Address of the guardian.
+    function guardian() external view returns (address);
+
+    /// @notice Global timelock duration in seconds for critical changes.
+    function timelock() external view returns (uint256);
+
+    /// @notice Pending `pendingGuardian` address and `validAt` timestamp when it becomes valid.
+    function pendingGuardian() external view returns (address pendingGuardian, uint64 validAt);
+
+    /// @notice Pending `newTimelock` duration and `validAt` timestamp when it becomes valid.
+    function pendingTimelock() external view returns (uint192 pendingTimelock, uint64 validAt);
+
+    /// @notice Configuration of a strategy connected to the vault.
+    /// @param strategy The address of the strategy.
+    /// @return cap The maximum asset allocation for the strategy.
+    /// @return enabled Whether the strategy is enabled or not.
+    /// @return proposed Whether the strategy is proposed or not (means it's waiting for approval).
+    /// @return lastAccrualTimestamp The timestamp of the last accrual.
+    /// @return strategyOwnerFeeBPS The percentage of performance fee that goes to the strategy owner.
+    /// @return lastTotalAssets The most recent total assets of the strategy.
+    function strategyConfig(address strategy) external view returns (
+        uint184 cap,
+        bool enabled,
+        bool proposed,
+        uint64 lastAccrualTimestamp,
+        uint96 strategyOwnerFeeBPS,
+        uint256 lastTotalAssets
+    );
+
     /// @notice Deposit `assets` underlying tokens on behalf of the `owner` and send in exchange the corresponding number of vault shares to `receiver`.
     /// This function is using a gasless transaction mechanism, that allows the `owner` to sign a permit signature off chain(using ERC2612) 
     /// before calling this function, and provide the signature components.
     ///
     /// This function can be used to allow relayers to deposit assets on behalf of the user.
+    ///
+    /// @dev The `owner` must sign a permit signature off chain before calling this function,
+    /// and provide the signature components.
+    ///
+    /// Important: This function can be called only if the underlying asset supports ERC2612 permit functionality.
     ///
     /// @param assets The amount of assets to deposit.
     /// @param owner The owner of the underlying assets.
@@ -24,12 +63,6 @@ interface IVault {
     /// @param permitV The v component of the signature.
     /// @param permitR The r component of the signature.
     /// @param permitS The s component of the signature.
-    ///
-    /// @dev The `owner` must sign a permit signature off chain before calling this function,
-    /// and provide the signature components.
-    ///
-    /// Important: This function can be called only if the underlying asset supports ERC2612 permit functionality.
-    ///
     /// @return The amount of shares the user will receive.
     function depositWithPermit(
         uint256 assets, 
@@ -47,6 +80,11 @@ interface IVault {
     ///
     /// This function can be used to allow relayers to mint shares on behalf of the user.
     ///
+    /// @dev The `owner` must sign a permit signature off chain before calling this function,
+    /// and provide the signature components.
+    ///
+    /// Important: This function can be called only if the underlying asset supports ERC2612 permit functionality.
+    ///
     /// @param shares The amount of shares to mint.
     /// @param owner The owner of the underlying assets.
     /// @param receiver The address to receive the shares.
@@ -54,12 +92,6 @@ interface IVault {
     /// @param permitV The v component of the signature.
     /// @param permitR The r component of the signature.
     /// @param permitS The s component of the signature.
-    ///
-    /// @dev The `owner` must sign a permit signature off chain before calling this function,
-    /// and provide the signature components.
-    ///
-    /// Important: This function can be called only if the underlying asset supports ERC2612 permit functionality.
-    ///
     /// @return The amount of assets the user will send.
     function mintWithPermit(
         uint256 shares, 
@@ -78,4 +110,63 @@ interface IVault {
     /// @notice Unpauses the vault.
     /// @dev Only the owner can unpause the vault.
     function unpause() external;
+
+    /// @notice Sets a new curator address.
+    /// @dev Only the owner can set a new curator address.
+    function setCurator(address newCurator) external;
+
+    /// @notice Submits a `newGuardian` address under timelock.
+    /// @dev Only the owner can submit a `newGuardian` guardian. 
+    /// The `newGuardian` can be accepted after the timelock has elapsed.
+    /// IMPORTANT: 
+    /// - The `newGuardian` address can not be the zero address.
+    /// - The `newGuardian` address must be different from the current guardian.
+    ///
+    /// @param newGuardian The new guardian address.
+    function submitGuardian(address newGuardian) external;
+
+    /// @notice Submits a `newTimelock` duration. Increases are instant, decreases are timelocked.
+    /// @dev Only the owner can call this function.
+    /// IMPORTANT: 
+    /// - The `newTimelock` duration can not be zero.
+    /// - If there is an active pending timelock (means not yet accepted), the function will revert.
+    ///
+    /// @param newTimelock The new timelock duration in seconds.
+    function submitTimelock(uint256 newTimelock) external;
+
+    /// @notice Sets a new performance fee for the vault in basis points.
+    /// @dev Only the owner can call this function.
+    /// IMPORTANT: 
+    /// - The `newFee` can not be greater than `MAX_FEE`.
+    /// - If the current `fee` is equal to the `newFee`, the function will revert.
+    ///
+    /// @param newFee The new performance fee in basis points.
+    function setFee(uint256 newFee) external;
+
+    /// @notice Sets a new performance fee recipient.
+    /// @dev Only the owner can call this function.
+    /// IMPORTANT: 
+    /// - The `newFeeRecipient` can not be the zero address.
+    /// - If the current `feeRecipient` is equal to the `newFeeRecipient`, the function will revert.
+    ///
+    /// @param newFeeRecipient The new performance fee recipient address.
+    function setFeeRecipient(address newFeeRecipient) external;
+
+    /// TODO: AWAITS FINISHING NATSPEC
+    /// @notice Accepts the pending guardian after the timelock has elapsed.
+    /// @dev Can be called by anyone.
+    function acceptGuardian() external;
+
+    /// TODO: AWAITS FINISHING NATSPEC
+    /// @notice Accepts a pending timelock decrease after the timelock has elapsed.
+    /// @dev Can be called by anyone.
+    function acceptTimelock() external;
+
+    /// TODO: AWAITS FINISHING NATSPEC
+    /// @notice Guardian can revoke a pending guardian change.
+    function revokePendingGuardian() external;
+
+    /// TODO: AWAITS FINISHING NATSPEC
+    /// @notice Guardian can revoke a pending timelock change.
+    function revokePendingTimelock() external;
 }
